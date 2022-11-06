c7a248d879e9be651dbc02e28c832069fdbf6ee8
Alex <longision@gmail.com>
2022-11-06 15:39:32 +0200

    New class and interface added: LocalizationResourceManager ILocalizationResourceManager

---
+++ uRecipes/Services/LocalisationSevice/ILocalizationResourceManager.cs
@@ -0,0 +1,13 @@
+﻿using System.Globalization;
+
+namespace uRecipes.Services.LocalisationSevice
+{
+    public interface ILocalizationResourceManager
+    {
+        object this[string resourceKey] { get; }
+
+        event PropertyChangedEventHandler PropertyChanged;
+
+        void SetCulture(CultureInfo culture);
+    }
+}
