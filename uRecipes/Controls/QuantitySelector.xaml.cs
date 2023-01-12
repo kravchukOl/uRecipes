a7645e1bd04b6dee2f928d11d5f9ba7258889ce8
Alex <longision@gmail.com>
2023-01-12 22:35:22 +0200

    Base features of QuantitySelector has been implemented

--- uRecipes/Controls/QuantitySelector.xaml.cs
+++ uRecipes/Controls/QuantitySelector.xaml.cs
@@ -1,9 +1,34 @@
+using System;
+
 namespace uRecipes.Controls;
 
 public partial class QuantitySelector : ContentView
 {
+	public static readonly BindableProperty QuantityProperty =
+		BindableProperty.Create(nameof(Quantity), 
+			typeof(int), 
+			typeof(QuantitySelector), 
+			defaultValue: 1,
+			defaultBindingMode: BindingMode.TwoWay,
+			propertyChanged: (bindable, oldvalue, newvalue) =>
+		{
+			var control = (QuantitySelector)bindable;
+			control.QuantityLabel.Text = newvalue?.ToString();
+		});
+
+	public int Quantity 
+	{
+		get => (int) GetValue(QuantityProperty); 
+		set => SetValue(QuantityProperty, (int) value);
+	}
 	public QuantitySelector()
 	{
 		InitializeComponent();
 	}
+
+	private void OnPlusTaped(object sender, TappedEventArgs e) => Quantity++;
+
+    private void OnMinusTaped(object sender, TappedEventArgs e) => Quantity--;
+
+
 }
