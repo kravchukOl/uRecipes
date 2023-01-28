92bdb66ee92eec4573766875aaf4502a4faafdbc
Alex <longision@gmail.com>
2023-01-29 00:58:47 +0200

    Recipe View  IngredientCatd Animation added

--- uRecipes/Views/RecipePage.xaml.cs
+++ uRecipes/Views/RecipePage.xaml.cs
@@ -1,20 +1,44 @@
 using Sharpnado.Tabs;
 using uRecipes.ViewModels;
-
+using uRecipes.Views.RecipePageViews;
 
 namespace uRecipes.Views;
 public partial class RecipePage : BasePage
 {
+    public bool isIngredientsHided = true;
+
+
 	public RecipePage(RecipeViewModel viewModel) : base(viewModel)
 	{
 		InitializeComponent();
+
 
 	}
     protected override void OnAppearing()
     {
         base.OnAppearing();
-        IngredientSheet.LoadView();
+        //IngredientSheet.LoadView();
         NutritionTable.LoadView();
         InstructionList.LoadView();
+    }
+
+    private void Ingredients_Tapped(object sender, TappedEventArgs e)
+    {
+        var animation = new CardsAnimation();
+        animation.Length = 1000;
+
+        if (isIngredientsHided) 
+        {
+            IngredientSheet.LoadView();
+            animation.Animate(IngredientSheet);
+            isIngredientsHided=false;
+        }
+        else
+        {
+            animation.AnimateBack(IngredientSheet);
+            isIngredientsHided = true;
+        }
+
+
     }
 }
---
+++ uRecipes/Views/RecipePageViews/CardsAnimation.cs
@@ -0,0 +1,37 @@
+﻿
+namespace uRecipes.Views.RecipePageViews
+{
+    public class CardsAnimation : CommunityToolkit.Maui.Animations.BaseAnimation
+    {
+
+        Animation OpenCard(VisualElement view)
+        {
+            var animation = new Animation();
+
+            animation.WithConcurrent((f) => view.HeightRequest = f, 0, 500);
+
+            return animation;
+        }
+
+        Animation CloseCard(VisualElement view)
+        {
+            var animation = new Animation();
+
+            animation.WithConcurrent((f) => view.HeightRequest = f, 500, 0);
+
+            return animation;
+        }
+
+        public override Task Animate(VisualElement view)
+        {
+            view.Animate("Card", OpenCard(view), 16, Length);
+            return Task.CompletedTask;
+        }
+
+        public Task AnimateBack(VisualElement view) 
+        {
+            view.Animate("Card", CloseCard(view), 16, Length);
+            return Task.CompletedTask;
+        }
+    }
+}
